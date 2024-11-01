eb0fe4d5ee49b820dab21433ecd8a23f1af5d267
Rahul Mandal <64683217+rahul-man@users.noreply.github.com>
2024-11-01 13:18:51 +0530

    Update ContainsDuplicate.java

--- hashmap/ContainsDuplicate.java
+++ hashmap/ContainsDuplicate.java
@@ -1,3 +1,5 @@
+/*https://leetcode.com/problems/contains-duplicate/description/*/
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         // create map to store element & frequency
