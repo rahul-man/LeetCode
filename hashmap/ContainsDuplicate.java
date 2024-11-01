642dcbc6552aa1f45db3bb49b03514372e55afe8
Rahul Mandal <mandalrahul52@gmail.com>
2024-11-01 13:17:46 +0530

    --contains duplicate

---
+++ hashmap/ContainsDuplicate.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        // create map to store element & frequency
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int ele : nums) {
+            // map doesn't contains element, store and updates frequency
+            if (!map.containsKey(ele)) {
+                map.put(ele, 1);
+            } else {
+                // map contains element, return true means array has duplicate element
+                return true;
+            }
+        }
+        // else return false
+        return false;
+    }
+}
